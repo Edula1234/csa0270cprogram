0c36035e004776dc37c457a1912bc86197e12295
Edula1234 <130446457+Edula1234@users.noreply.github.com>
2023-04-17 22:45:38 +0530

    Update 17ass10.c

--- 17ass10.c
+++ 17ass10.c
@@ -3,28 +3,25 @@
 
 using namespace std;
 
-// Define the union shape
 union Shape {
-    double radius;  // Circle's radius
+    double radius;  
     struct {
-        double length;  // Rectangle's length
-        double width;   // Rectangle's width
+        double length;  
+        double width;   
     } rectangle;
 };
 
 int main() {
-    Shape shape;  // Declare a variable of type Shape
-    
-    // Prompt the user to enter the type of shape
+    Shape shape;  
     char choice;
     cout << "Enter shape (c for circle, r for rectangle): ";
     cin >> choice;
     
-    // Based on user's choice, prompt for appropriate dimensions
+    
     if (choice == 'c') {
         cout << "Enter circle's radius: ";
         cin >> shape.radius;
-        // Calculate and print the area of the circle
+         
         double area = M_PI * pow(shape.radius, 2);
         cout << "Area of the circle: " << area << endl;
     } else if (choice == 'r') {
@@ -32,7 +29,7 @@ int main() {
         cin >> shape.rectangle.length;
         cout << "Enter rectangle's width: ";
         cin >> shape.rectangle.width;
-        // Calculate and print the area of the rectangle
+        
         double area = shape.rectangle.length * shape.rectangle.width;
         cout << "Area of the rectangle: " << area << endl;
     } else {
